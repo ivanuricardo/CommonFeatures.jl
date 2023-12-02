391f0c889718c6d3bb690ac6b17cfa173a3731b2
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-12-02 15:49:41 +0100

    add companion, isstable functions

--- src/TensorEconometrics.jl
+++ src/TensorEconometrics.jl
@@ -6,8 +6,10 @@ using LinearAlgebra
 
 export tensorols, art
 export tlag, ridgerankselect
+export makecompanion, isstable
 include("./Regressions/MatrixAR.jl")
 include("./Regressions/utils.jl")
+include("./Regressions/companionmatrix.jl")
 
 
 end
