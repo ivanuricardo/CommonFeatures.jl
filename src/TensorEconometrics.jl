8f01086a8f8ea97e00a43f6dbf903616a6429ff3
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-12-02 13:09:53 +0100

    first package

--- src/TensorEconometrics.jl
+++ src/TensorEconometrics.jl
@@ -1,5 +1,16 @@
 module TensorEconometrics
 
-# Write your package code here.
+export greet
+
+"""
+greet: prints hello!
+
+params:
+------
+    - None!
+"""
+function greet()
+    println("Hello!")
+end
 
 end
