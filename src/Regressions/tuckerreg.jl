aed82dc538795615a29a8a7e0bfe50d1f14a93a7
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-04-24 17:58:14 +0200

    Fix gradient function

--- src/Regressions/tuckerreg.jl
+++ src/Regressions/tuckerreg.jl
@@ -27,9 +27,7 @@ function gradU(whichU::Int,
     Y::AbstractArray,
     X::AbstractArray,
     G::AbstractArray,
-    p::Int,
     U::Vector{<:AbstractMatrix})
-    N1, N2, _ = size(Y)
     dlbar = dlbarest(Y, X, G, U)
 
     kronU_dict = Dict(
@@ -40,7 +38,7 @@ function gradU(whichU::Int,
     )
 
     kronU = kronU_dict[whichU]
-    ∇U = reshape(dlbar, (N1, N2 * N1 * N2 * p)) * kronU
+    ∇U = tenmat(dlbar, row=whichU) * kronU
     return ∇U
 end
 
@@ -64,12 +62,12 @@ Uses the gradient descent algorithm of Wang, Zhang, and Li 2024.
 # Arguments
 - `mardata::AbstractArray`: Input tensor data. Should be ``N1 \times N2 \times T``
 - `ranks::AbstractVector`: Vector specifying the desired ranks of the Tucker decomposition.
-- `eta::AbstractFloat`: Learning rate for gradient descent (default: 1e-04).
+- `eta::AbstractFloat`: Learning rate for gradient descent (default: 1e-03).
 - `a::Real`: Regularization parameter (default: 1).
 - `b::Real`: Regularization parameter (default: 1).
 - `maxiter::Int`: Maximum number of iterations for gradient descent (default: 3000).
 - `p::Int`: Number of lags to include
-- `ϵ::AbstractFloat`: Convergence threshold for stopping criteria (default: 1e-04).
+- `ϵ::AbstractFloat`: Convergence threshold for stopping criteria (default: 1e-03).
 
 # Returns
 A tuple containing the Tucker decomposition components:
@@ -82,7 +80,7 @@ A tuple containing the Tucker decomposition components:
 function tuckerreg(
     mardata::AbstractArray,
     ranks::AbstractVector,
-    eta::Real=1e-02,
+    eta::Real=1e-03,
     maxiter::Int=500,
     p::Int=1,
     ϵ::AbstractFloat=1e-03,
@@ -114,27 +112,27 @@ function tuckerreg(
     for s in 1:maxiter
         iters += 1
 
-        ∇U1 = gradU(1, cenorig, cenlag, G, p, U)
-        U1 -= eta * ∇U1
+        ∇U1 = gradU(1, cenorig, cenlag, G, U)
+        U[1] -= eta * ∇U1
         trackU1[s] = norm(∇U1)
 
-        ∇U2 = gradU(2, cenorig, cenlag, G, p, U)
-        U2 -= eta * ∇U2
+        ∇U2 = gradU(2, cenorig, cenlag, G, U)
+        U[2] -= eta * ∇U2
         trackU2[s] = norm(∇U2)
 
-        ∇U3 = gradU(3, cenorig, cenlag, G, p, U)
-        U3 -= eta * ∇U3
+        ∇U3 = gradU(3, cenorig, cenlag, G, U)
+        U[3] -= eta * ∇U3
         trackU3[s] = norm(∇U3)
 
-        ∇U4 = gradU(4, cenorig, cenlag, G, p, U)
-        U4 -= eta * ∇U4
+        ∇U4 = gradU(4, cenorig, cenlag, G, U)
+        U[4] -= eta * ∇U4
         trackU4[s] = norm(∇U4)
 
         ∇G = gradG(cenorig, cenlag, G, U)
         trackG[s] = norm(∇G)
         G -= eta * ∇G
 
-        A = full(ttensor(G, [U1, U2, U3, U4, Matrix(U5)]))
+        A = full(ttensor(G, U))
 
         # Stopping Condition
         if s > 1
