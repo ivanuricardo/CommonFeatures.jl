09b47d4acc220e1f4831b8983cfdd454f3ed884a
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-09-25 10:51:58 +0200

    replacing y with mardata in grads

--- src/Regressions/mecm.jl
+++ src/Regressions/mecm.jl
@@ -119,43 +119,43 @@ function mecm(
 
         iters += 1
 
-        ∇D = mecmsumres(ΔY, Y, U1, U2, U3, U4, ϕ1, ϕ2, D)
+        ∇D = mecmsumres(ΔY, mardata, U1, U2, U3, U4, ϕ1, ϕ2, D)
         etaD = 1 / obs
         D += etaD * ∇D
         trackD[s] = etaD
 
-        ∇U1 = U1grad(ΔY, Y, U1, U2, U3, U4, ϕ1, ϕ2, D)
-        hU1 = U1hessian(Y, U2, U3, U4)
+        ∇U1 = U1grad(ΔY, mardata, U1, U2, U3, U4, ϕ1, ϕ2, D)
+        hU1 = U1hessian(mardata, U2, U3, U4)
         etaU1 = 1 / (maximum(abs.(eigvals(hU1))))
         U1 += etaU1 * ∇U1
         trackU1[s] = etaU1
 
-        ∇U2 = U2grad(ΔY, Y, U1, U2, U3, U4, ϕ1, ϕ2, D)
-        hU2 = U2hessian(Y, U1, U3, U4)
+        ∇U2 = U2grad(ΔY, mardata, U1, U2, U3, U4, ϕ1, ϕ2, D)
+        hU2 = U2hessian(mardata, U1, U3, U4)
         etaU2 = 1 / (maximum(abs.(eigvals(hU2))))
         U2 += etaU2 * ∇U2
         trackU2[s] = etaU2
 
-        ∇U3 = U3grad(ΔY, Y, U1, U2, U3, U4, ϕ1, ϕ2, D)
-        hU3 = U3hessian(Y, U1, U2, U4)
+        ∇U3 = U3grad(ΔY, mardata, U1, U2, U3, U4, ϕ1, ϕ2, D)
+        hU3 = U3hessian(mardata, U1, U2, U4)
         etaU3 = 1 / (maximum(abs.(eigvals(hU3))))
         U3 += etaU3 * ∇U3
         trackU3[s] = etaU3
 
-        ∇U4 = U4grad(ΔY, Y, U1, U2, U3, U4, ϕ1, ϕ2, D)
-        hU4 = U4hessian(Y, U1, U2, U3)
+        ∇U4 = U4grad(ΔY, mardata, U1, U2, U3, U4, ϕ1, ϕ2, D)
+        hU4 = U4hessian(mardata, U1, U2, U3)
         etaU4 = 1 / (maximum(abs.(eigvals(hU4))))
         U4 += etaU4 * ∇U4
         trackU4[s] = etaU4
 
         if p != 0
-            ∇ϕ1 = ϕ1grad(ΔY, Y, U1, U2, U3, U4, ϕ1, ϕ2, D)
+            ∇ϕ1 = ϕ1grad(ΔY, mardata, U1, U2, U3, U4, ϕ1, ϕ2, D)
             hϕ1 = ϕ1hessian(ΔY, ϕ2)
             etaϕ1 = 1 / (maximum(abs.(eigvals(hϕ1))))
             ϕ1 += etaϕ1 * ∇ϕ1
             trackϕ1[s] = norm(∇ϕ1)
 
-            ∇ϕ2 = ϕ2grad(ΔY, Y, U1, U2, U3, U4, ϕ1, ϕ2, D)
+            ∇ϕ2 = ϕ2grad(ΔY, mardata, U1, U2, U3, U4, ϕ1, ϕ2, D)
             hϕ2 = ϕ2hessian(ΔY, ϕ1)
             etaϕ2 = 1 / (maximum(abs.(eigvals(hϕ2))))
             ϕ2 += etaϕ2 * ∇ϕ2
