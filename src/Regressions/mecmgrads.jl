ca21c89f7d42fad18ed6a591d1a6eafe11bf61e2
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-09-27 10:42:29 +0200

    trying s1

--- src/Regressions/mecmgrads.jl
+++ src/Regressions/mecmgrads.jl
@@ -214,6 +214,6 @@ function Σ1grad(Y, U1, U2, U3, U4, Σ1, Σ2, ϕ1, ϕ2, D)
         res = ΔY - U1U3 * Y[:, :, (i-1)] * U2U4' - phiY - D
         totsum += res * iS2 * res'
     end
-    return (obs / 2) * Σ1 - 0.5 .* totsum
+    return (obs / 2) .* Σ1 - 0.5 .* totsum
 end
 
