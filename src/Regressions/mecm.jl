e697c415b88bfbffb026fdb35e259336e18693cb
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-10-11 12:23:13 +0200

    faster start?

--- src/Regressions/mecm.jl
+++ src/Regressions/mecm.jl
@@ -124,11 +124,11 @@ function mecm(
 
     iters = 0
 
-    for k in 0:99
-        newΣ1 = (0.01 * k + 0.01) * I(N1)
-        newΣ2 = (0.01 * k + 0.01) * I(N2)
+    for k in 0:49
+        newΣ1 = (0.02 * k + 0.01) * I(N1)
+        newΣ2 = (0.02 * k + 0.01) * I(N2)
         savell = fill(NaN, 10)
-        for m in 1:10
+        for m in 1:5
 
             ∇D = mecmsumres(mardata, U1, U2, U3, U4, newΣ1, newΣ2, ϕ1, ϕ2, D)
             etaD = 1 / spectralradius((obs) * kron(inv(newΣ2), inv(newΣ1)))
