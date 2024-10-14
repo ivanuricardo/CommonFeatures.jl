a4b021a7484a14706d5d19e5f72d211d572d8b7d
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-10-14 13:30:57 +0200

    rearrange break

--- src/Regressions/mecm.jl
+++ src/Regressions/mecm.jl
@@ -181,12 +181,12 @@ function mecm(
                 etaϕ2 = 1 / spectralradius(hϕ2)
                 ϕ2 += etaϕ2 * ∇ϕ2
             end
-            savell[m] = matobj(mardata, D, U1, U2, U3, U4, newΣ1, newΣ2, ϕ1, ϕ2)
             if m > 1
                 if savell[m] < savell[m-1]
                     break
                 end
             end
+            savell[m] = matobj(mardata, D, U1, U2, U3, U4, newΣ1, newΣ2, ϕ1, ϕ2)
         end
         newobj = matobj(mardata, D, U1, U2, U3, U4, newΣ1, newΣ2, ϕ1, ϕ2)
         if newobj > oldobj
