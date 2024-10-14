62bb14915ca2566fb4283d876b592d1450962d3e
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-10-14 21:59:51 +0200

    adjust reg term

--- src/Regressions/mecm.jl
+++ src/Regressions/mecm.jl
@@ -149,7 +149,7 @@ function mecm(
             # newΣ1unscaled = newΣ1 + etaS * ∇newΣ1
             preΣ1 = newΣ1 + etaS * ∇newΣ1
             eΣ1 = eigen(preΣ1)
-            newΣ1unscaled = eΣ1.vectors * diagm(max.(real.(eΣ1.values), 1e-04)) * eΣ1.vectors'
+            newΣ1unscaled = eΣ1.vectors * diagm(max.(real.(eΣ1.values), 1e-03)) * eΣ1.vectors'
             newΣ1norm = newΣ1unscaled ./ norm(newΣ1unscaled)
             enorm = eigen(newΣ1norm)
             newΣ1 = enorm.vectors * diagm(max.(real.(enorm.values), 1e-15)) * enorm.vectors'
@@ -169,7 +169,7 @@ function mecm(
             ∇newΣ2 = Σ2grad(mardata, U1, U2, U3, U4, newΣ1, newΣ2, ϕ1, ϕ2, D)
             preΣ2 = newΣ2 + etaS * ∇newΣ2
             eΣ2 = eigen(preΣ2)
-            newΣ2 = eΣ2.vectors * diagm(max.(real.(eΣ2.values), 1e-04)) * eΣ2.vectors'
+            newΣ2 = eΣ2.vectors * diagm(max.(real.(eΣ2.values), 1e-03)) * eΣ2.vectors'
             # newΣ2 += etaS * ∇newΣ2
             newΣ2 = (newΣ2 + newΣ2') / 2
 
@@ -219,7 +219,7 @@ function mecm(
         ∇Σ1 = Σ1grad(mardata, U1, U2, U3, U4, Σ1, Σ2, ϕ1, ϕ2, D)
         preΣ1 = Σ1 + etaS * ∇Σ1
         eΣ1 = eigen(preΣ1)
-        Σ1unscaled = eΣ1.vectors * diagm(max.(real.(eΣ1.values), 1e-04)) * eΣ1.vectors'
+        Σ1unscaled = eΣ1.vectors * diagm(max.(real.(eΣ1.values), 1e-03)) * eΣ1.vectors'
         # Σ1unscaled = Σ1 + etaS * ∇Σ1
         Σ1norm = Σ1unscaled ./ norm(Σ1unscaled)
         enorm = eigen(Σ1norm)
@@ -243,7 +243,7 @@ function mecm(
         # Σ2 += etaS * ∇Σ2
         preΣ2 = Σ2 + etaS * ∇Σ2
         eΣ2 = eigen(preΣ2)
-        Σ2 = eΣ2.vectors * diagm(max.(real.(eΣ2.values), 1e-04)) * eΣ2.vectors'
+        Σ2 = eΣ2.vectors * diagm(max.(real.(eΣ2.values), 1e-03)) * eΣ2.vectors'
         Σ2 = (Σ2 + Σ2') / 2
 
         if p != 0
