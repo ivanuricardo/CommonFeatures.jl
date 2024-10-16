d1c32ff5090dbf5d7d974ff778d489f89f22b789
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-10-16 22:24:06 +0200

    slightly more

--- src/Regressions/mecm.jl
+++ src/Regressions/mecm.jl
@@ -101,7 +101,7 @@ function mecm(
     ranks::AbstractVector;
     p::Int=0,
     maxiter::Int=100,
-    etaS::AbstractFloat=4e-08,
+    etaS::AbstractFloat=5e-08,
     ϵ::AbstractFloat=1e-02
 )
     if length(ranks) != 2
