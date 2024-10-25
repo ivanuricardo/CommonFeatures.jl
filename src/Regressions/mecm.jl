6a955c1c11475681ec8e2e458b9f4751c8994979
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-10-25 16:41:41 +0200

    trying with U1 norm

--- src/Regressions/mecm.jl
+++ src/Regressions/mecm.jl
@@ -134,7 +134,8 @@ function mecm(
         ∇U1 = U1grad(mardata, U1, U2, U3, U4, Σ1, Σ2, ϕ1, ϕ2, D)
         hU1 = U1hessian(mardata, U2, U3, U4, Σ1, Σ2)
         etaU1 = 1 / spectralradius(hU1)
-        U1 += etaU1 * ∇U1
+        U1unscaled = U1 + etaU1 * ∇U1
+        U1 = U1unscaled ./ norm(U1unscaled)
         trackU1[s] = etaU1
 
         ∇U3 = U3grad(mardata, U1, U2, U3, U4, Σ1, Σ2, ϕ1, ϕ2, D)
